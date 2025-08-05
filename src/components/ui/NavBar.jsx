fca21e5413aec18a3ddd7fb02685f143be881c36
Daniel-tech-sys <danielrafael9729@gmail.com>
2025-08-04 19:09:50 -0500

    ProyectoFinal+Alba - Update Rutas

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -126,7 +126,7 @@ export function NavBar() {
           ))}
 
           <NavLink
-            to="/checkout"
+            to="/compras"
             onClick={() => setIsOpen(false)}
             className="block rounded-md px-3 py-2 text-base font-medium text-gray-700"
           >
